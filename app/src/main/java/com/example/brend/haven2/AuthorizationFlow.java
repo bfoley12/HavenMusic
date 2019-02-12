3eec043f5d72d7ebad0428d480f19b8593bbdad1
bfoley12 <brendanfoley@optonline.net>
2019-02-12 17:52:34 -0500

    Touched up Login authorization flow. Now properly checks for valid email-password pair before proceeding.

--- app/src/main/java/com/example/brend/haven2/AuthorizationFlow.java
+++ app/src/main/java/com/example/brend/haven2/AuthorizationFlow.java
@@ -1,12 +1,16 @@
 package com.example.brend.haven2;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -86,11 +90,24 @@ public class AuthorizationFlow extends AppCompatActivity {
     public void logIn(Bundle extras){
         Log.d("DEBUG", "AuthorizationFlow logIn");
 
-        mAuth.signInWithEmailAndPassword(
-                extras.getString("USERNAME"),extras.getString("PASSWORD"));
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-
+        Task task = mAuth.signInWithEmailAndPassword(
+                extras.getString("USERNAME"),extras.getString("PASSWORD")).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("DEBUG", "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.d("DEBUG", "signInWithEmail:failure", task.getException());
+                    Toast.makeText(AuthorizationFlow.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
     }
     public void logOut(){
         Log.d("DEBUG", "AuthorizationFlow logOut");
